74ba2881587ba8adb269c799565fc91defc3c581
Josh Leder <josh@ha.cr>
2015-04-23 14:35:10 -0600

    add prerequisite to capistranos task

--- lib/capistrano/default_stage.rb
+++ lib/capistrano/default_stage.rb
@@ -1,10 +1,15 @@
 require "capistrano/default_stage/version"
 
-Rake::Task.define_task(:ensure_stage)
-
+# If the stage is not set on the command line, this
+# task will set it from the configured default_stage.
 Rake::Task.define_task(:default_stage) do 
+  # neuter capistrano's 'ensure_stage'
+  Rake::Task[:ensure_stage].clear_actions
+
   unless stage_set?
     default_stage = fetch(:default_stage)
+
+    # fail loudly when no default_stage is set
     unless default_stage
       puts t(:stage_not_set)
       exit 1
@@ -14,3 +19,7 @@ Rake::Task.define_task(:default_stage) do
     invoke default_stage
   end
 end
+
+# Refine capistrano's 'ensure_stage' task to 
+# depend on our 'default_stage' task.
+Rake::Task.define_task(:ensure_stage => :default_stage)
